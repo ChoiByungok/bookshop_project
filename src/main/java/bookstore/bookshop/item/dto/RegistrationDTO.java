afa7ab5667bd106d4a31ef9158bf4843903bf8f3
CBO <quddhr9523@naver.com>
2023-10-17 15:32:29 +0900

    feat: dto 클래스 생성

---
+++ src/main/java/bookstore/bookshop/item/dto/RegistrationDTO.java
@@ -0,0 +1,22 @@
+package bookstore.bookshop.item.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class RegistrationDTO {
+    String type;
+    String itemName;
+    Integer price;
+    Integer stockQuantity;
+    String categoryName;
+    String artist;
+    String etc;
+    String author;
+    String isbn;
+    String actor;
+    String director;
+}
