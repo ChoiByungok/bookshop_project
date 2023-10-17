04b04f99652b40fc9246174633a36756dad951b7
CBO <quddhr9523@naver.com>
2023-10-17 15:31:13 +0900

    rename: repository 이름 바꿈

--- src/main/java/bookstore/bookshop/item/repository/ItemQueryRepository.java
+++ src/main/java/bookstore/bookshop/item/repository/ItemQueryRepository.java
@@ -1,7 +1,6 @@
 package bookstore.bookshop.item.repository;
 
 import bookstore.bookshop.domain.item.Item;
-import bookstore.bookshop.domain.item.QItem;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -12,7 +11,7 @@ import static bookstore.bookshop.domain.item.QItem.item;
 
 @Repository
 @RequiredArgsConstructor
-public class ItemRepository {
+public class ItemQueryRepository {
 
     private final JPAQueryFactory queryFactory;
 
