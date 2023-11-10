b863e682f6a8503ad17fe80ab8f7a552e28a3b41
CBO <quddhr9523@naver.com>
2023-11-10 16:33:04 +0900

    feat: 기본생성자 어노테이션 추가

--- src/main/java/bookstore/bookshop/domain/item/Movie.java
+++ src/main/java/bookstore/bookshop/domain/item/Movie.java
@@ -2,6 +2,7 @@ package bookstore.bookshop.domain.item;
 
 import bookstore.bookshop.item.dto.RegistrationDTO;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.DiscriminatorValue;
@@ -11,6 +12,7 @@ import javax.persistence.Entity;
 @DiscriminatorValue("M")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Movie extends Item{
 
     private String director;
